931b77292f51b48a52cff8592bb2745aefa5e670
bengin34 <becaglar3434@gmail.com>
2023-02-13 23:41:07 +0300

    bsc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 // import logo from './logo.svg';
-import "./App.css";
 // import Header from "./Component/Header";
 
-import Form from "./Component/Form/Form";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-const data = {
-  type:["text","text", "email"],
-  placeholder:["firstname","lastname","email"]
-}
+
 
   return(
     <div>
-      <Form type={data.type} placeholder={data.firstname}/>
+
     </div>
   )
   };
