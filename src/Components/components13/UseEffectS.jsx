918c00f0a108c79567a3a4edfca3012e5e33f938
selim-haspolat <selimhaspolat@gmail.com>
2023-02-22 11:04:28 +0300

    clg farkı

--- src/Components/components13/UseEffectS.jsx
+++ src/Components/components13/UseEffectS.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UseEffectS = () => {
   const [message, setMessage] = useState("hello");
   const [count, setCount] = useState(0);
 
+
+  
+  useEffect(() => {
+      const header = document.getElementById('header')
+      console.log(header);
+
+      console.log('effect');
+    },[message, count])
+    
+
   return (
     <div>
-      <h1 className="text-center text-slate-700 text-5xl font-light uppercase my-5 border-b border-slate-500 py-2">
+      <h1 id="header" className="text-center text-slate-700 text-5xl font-light uppercase my-5 border-b border-slate-500 py-2">
         Effect{" "}
       </h1>
 
@@ -21,9 +31,9 @@ const UseEffectS = () => {
       <br />
       <br />
 
-      <span className="m-2">0</span>
-      <button className="border text-indigo-500 border-indigo-500 hover:bg-indigo-500 hover:text-white px-5 py-1 rounded-lg transition-colors">
-        change
+      <span className="m-2">{count}</span>
+      <button onClick={() => setCount(count + 1)} className="border text-indigo-500 border-indigo-500 hover:bg-indigo-500 hover:text-white px-5 py-1 rounded-lg transition-colors">
+        inc
       </button>
     </div>
   );
