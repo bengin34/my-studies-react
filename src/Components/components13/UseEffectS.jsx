a06226a1f6a8b15f20ed6a5394fd1a7c47ac89ce
selim-haspolat <selimhaspolat@gmail.com>
2023-02-22 10:53:17 +0300

    .

--- src/Components/components13/UseEffectS.jsx
+++ src/Components/components13/UseEffectS.jsx
@@ -1,13 +1,17 @@
+
+
 const UseEffectS = () => {
+
+
+
   return (
     <div>
       <h1 className="text-center text-slate-700 text-5xl font-light uppercase my-5 border-b border-slate-500 py-2">
         Effect{" "}
       </h1>
 
-      <span className="m-2">{message}</span>
+      <span className="m-2"></span>
       <button
-        onClick={() => setMessage("hi")}
         className="border text-green-500 border-green-500 hover:bg-green-500 hover:text-white px-5 py-1 rounded-lg transition-colors"
       >
         change
@@ -16,9 +20,8 @@ const UseEffectS = () => {
       <br />
       <br />
 
-      <span className="m-2">{count}</span>
+      <span className="m-2"></span>
       <button
-        onClick={() => setCount(count + 1)}
         className="border text-indigo-500 border-indigo-500 hover:bg-indigo-500 hover:text-white px-5 py-1 rounded-lg transition-colors"
       >
         change
