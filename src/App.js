f63fe5a2ffc03195b8fcdb4b4bbee4c0a06e552e
selim-haspolat <selimhaspolat@gmail.com>
2023-02-22 11:11:10 +0300

    toogle component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import UseEffectS from "./Components/components13/UseEffectS";
 
-
 function App() {
+  const [toggleComponent, setToggleComponent] = useState(true);
+
   return (
     <div>
-      <UseEffectS/>
+      <button onClick={() => setToggleComponent(!toggleComponent)}>
+        toggle component
+      </button>
+      {toggleComponent && <UseEffectS />}
     </div>
   );
 }
--- src/Components/components13/UseEffectS.jsx
+++ src/Components/components13/UseEffectS.jsx
@@ -3,20 +3,45 @@ import { useEffect, useState } from "react";
 const UseEffectS = () => {
   const [message, setMessage] = useState("hello");
   const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(0)
 
 
-  
-  useEffect(() => {
-      const header = document.getElementById('header')
-      console.log(header);
+//   useEffect(() => {
+//     const header = document.getElementById("header");
+//     console.log(header);
 
-      console.log('effect');
-    },[message, count])
+//     console.log("effect");
+//   }, [message, count]);
+
+
+    // useEffect(() => {
+    //     console.log('did Mount');
+    // }, [])
+
+
+    useEffect(() => {
+        const myTimer = setInterval(() => {
+            setTimer(timer => timer + 1)
+        }, 1000);
+      return () => {
+        clearInterval(myTimer)
+      }
+    }, [])
+
+    useEffect(() => {
+        console.log('timer: ', timer);
+    }, [timer])
     
+    
+    
+
 
   return (
     <div>
-      <h1 id="header" className="text-center text-slate-700 text-5xl font-light uppercase my-5 border-b border-slate-500 py-2">
+      <h1
+        id="header"
+        className="text-center text-slate-700 text-5xl font-light uppercase my-5 border-b border-slate-500 py-2"
+      >
         Effect{" "}
       </h1>
 
@@ -32,7 +57,10 @@ const UseEffectS = () => {
       <br />
 
       <span className="m-2">{count}</span>
-      <button onClick={() => setCount(count + 1)} className="border text-indigo-500 border-indigo-500 hover:bg-indigo-500 hover:text-white px-5 py-1 rounded-lg transition-colors">
+      <button
+        onClick={() => setCount(count + 1)}
+        className="border text-indigo-500 border-indigo-500 hover:bg-indigo-500 hover:text-white px-5 py-1 rounded-lg transition-colors"
+      >
         inc
       </button>
     </div>
